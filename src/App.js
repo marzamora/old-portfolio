6a8ecd7b515139a213c60ca81dc0755cd2294672
Marcello Zamora <zamoramaj@gmail.com>
2021-01-25 19:58:02 -0500

    added icons for use globally

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Showcase from './components/Showcase';
+/* ------------------------------------------
+* For using icons - see link for more info
+* https://fontawesome.com/how-to-use/on-the-web/using-with/react
+* fab repsresents all brand icons in free-brands-svg-icons
+*/
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, faChevronDown);
 
 function App() {
   return (
